0b0524ffacdab6348c58ae73ba9126ca6c81d514
Black Funny <184131195@qq.com>
2020-08-31 16:19:09 +0800

    Delete all annotations
    删除了所有的注释。

--- src/main/java/com/fannyuan/bookmanager/controllers/BookController.java
+++ src/main/java/com/fannyuan/bookmanager/controllers/BookController.java
@@ -7,8 +7,6 @@ import com.fannyuan.bookmanager.service.BookService;
 import com.fannyuan.bookmanager.service.HostHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,21 +20,17 @@ public class BookController {
     @Resource
     private HostHolder hostHolder;
 
-//    @RequestMapping(path = {"/index"}, method = {RequestMethod.GET})
     @GetMapping("/index")
     public List<Book> bookList() {
         User host = hostHolder.getUser();
 
         if (host != null) {
-//            model.addAttribute("host", host);
             return bookService.getAllBooks();
         } else {
             return null;
         }
-//        loadAllBooksView(model);
     }
 
-//    @RequestMapping(path = {"/books/add"}, method = {RequestMethod.POST})
     @PostMapping(path = "/books/add",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -53,21 +47,15 @@ public class BookController {
         }
     }
 
-//    @RequestMapping(path = {"/books/{bookId:[0-9]+}/delete"}, method = {RequestMethod.GET})
     @GetMapping(path = "/books/{bookId:[0-9]+}/delete")
     public HttpStatus deleteBook(@PathVariable("bookId") int bookId) {
         bookService.deleteBooks(bookId);
         return HttpStatus.OK;
     }
 
-//    @RequestMapping(path = {"/books/{bookId:[0-9]+}/recover"}, method = {RequestMethod.GET})
     @GetMapping(path = "/books/{bookId:[0-9]+}/recover")
     public HttpStatus recoverBook(@PathVariable("bookId") int bookId) {
         bookService.recoverBooks(bookId);
         return HttpStatus.OK;
     }
-
-//    private void loadAllBooksView(Model model) {
-//        model.addAttribute("books", bookService.getAllBooks());
-//    }
 }
--- src/main/java/com/fannyuan/bookmanager/controllers/LoginController.java
+++ src/main/java/com/fannyuan/bookmanager/controllers/LoginController.java
@@ -3,35 +3,20 @@ package com.fannyuan.bookmanager.controllers;
 import com.fannyuan.bookmanager.biz.LoginBiz;
 import com.fannyuan.bookmanager.controllers.request.NewUser;
 import com.fannyuan.bookmanager.model.User;
-import com.fannyuan.bookmanager.service.UserService;
 import com.fannyuan.bookmanager.utils.CookieUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
-@Slf4j
 @RestController
 @RequestMapping("/users")
 public class LoginController {
     @Resource
     private LoginBiz loginBiz;
 
-    @Resource
-    private UserService userService;
-
-//    @RequestMapping(path = {"/users/register"}, method = {RequestMethod.GET})
-//    public String register() {
-//        return "login/register";
-//    }
-
-//    @RequestMapping(path = {"/users/register/do"}, method = {RequestMethod.POST})
     @PostMapping(path = "/register/do",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -49,17 +34,10 @@ public class LoginController {
             CookieUtils.writeCookie("ticket", ticket, response);
             return HttpStatus.OK;
         } catch (Exception e) {
-            log.info(String.valueOf(e));
             return HttpStatus.BAD_REQUEST;
         }
     }
 
-//    @RequestMapping(path = {"/users/login"}, method = {RequestMethod.GET})
-//    public String login() {
-//        return "login/login";
-//    }
-
-//    @RequestMapping(path = {"/users/login/do"}, method = {RequestMethod.POST})
     @PostMapping(path = "/login/do",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -72,13 +50,11 @@ public class LoginController {
             CookieUtils.writeCookie("ticket", ticket, response);
             return HttpStatus.OK;
         } catch (Exception e) {
-//            model.addAttribute("error", e.getMessage());
 
             return HttpStatus.BAD_REQUEST;
         }
     }
 
-//    @RequestMapping(path = {"/users/logout"}, method = {RequestMethod.GET})
     @GetMapping(path = "/logout")
     public HttpStatus logout(@CookieValue("ticket") String ticket) {
         loginBiz.logout(ticket);
